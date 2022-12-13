b2c32f66cc95a4ab4215ef917c05b3a1678e6052
Echchaoui <kurra9904@gmail.com>
2022-12-13 11:17:41 +0100

    first commit

---
+++ app/controllers/homeControllers.php
@@ -0,0 +1,11 @@
+<?php
+
+class homeControllers{
+    public function index() {
+        echo "this is function index";
+    }
+
+    public function login() {
+        echo "this is function login";
+    }
+}
